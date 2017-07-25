87869ef1643422b2294f986dad8df92050558bd4
Akinori ABE <aabe.65535@gmail.com>
2017-07-25 15:33:36 +0900

    :pencil2: write README.md

--- git/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eu
-
-git_topdir=$(git rev-parse --show-toplevel 2>/dev/null)
-git_gitdir=$(pwd)/$(git rev-parse --git-dir 2>/dev/null)
-
-if [[ "${git_topdir}" != '' ]]; then
-	ln -f "${git_topdir}/git/pre-commit" "${git_gitdir}/hooks/pre-commit"
-fi
