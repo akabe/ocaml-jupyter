197b27e019cabc5e39e465e446f1c5e28c2e7cc6
Akinori ABE <abe_akinori@cyberagent.co.jp>
2018-06-25 15:29:19 +0900

    :-1: remove notebook and comm tests
    They are instable tests because paths of object files under
    _build directory depends on jbuilder versions.

--- test/fixtures/nbinit.ml
+++
@@ -1,7 +0,0 @@
-#use "topfind" ;;
-Topfind.log := ignore ;;
-#require "yojson,base64,uuidm" ;;
-#directory "../jupyter/src/core" ;;
-#directory "../jupyter/src/notebook" ;;
-#load "../jupyter/src/core/jupyter.cma" ;;
-#load "../jupyter/src/notebook/jupyter_notebook.cma" ;;
