d39e3c3a5ec1c475305a6bdfa54fa78dc7d6890a
Akinori ABE <abe_akinori@cyberagent.co.jp>
2018-10-09 16:22:23 +0900

    :bug: support multiple-version OCaml

--- config/kernelspec.ml
+++ config/kernelspec.ml
@@ -1,4 +1,4 @@
-(** Generate kernel.json *)
+(** Generate kernel.sh and kernel.json *)
 
 open Format
 
@@ -9,14 +9,15 @@ type kernelspec =
     argv : string list;
   } [@@deriving to_yojson]
 
-let main ~output ~bindir ~home =
+let write_kernelspec_json ~output ~bindir ~sharedir ~switch ~home =
   let oc = open_out output in
-  let display_name = sprintf "OCaml %s" Sys.ocaml_version in
+  let display_name = sprintf "OCaml %s" switch in
   `Assoc [
     "display_name", `String display_name;
     "language", `String "OCaml";
     "argv", `List [
-      `String (Filename.concat bindir "ocaml-jupyter-kernel");
+      `String "/bin/sh";
+      `String (Filename.(concat sharedir (concat "jupyter" "kernel.sh")));
       `String "--init";
       `String (Filename.concat home ".ocamlinit");
       `String "--merlin";
@@ -30,6 +31,17 @@ let main ~output ~bindir ~home =
   |> Yojson.Safe.pretty_to_channel oc ;
   close_out oc
 
+let write_kernel_sh ~output ~bindir ~switch =
+  let oc = open_out output in
+  sprintf "\
+  #!/bin/sh\n\
+  \n\
+  eval `opam config env %S` && %S \"$@\"\n"
+    ("--switch=" ^ switch)
+    (Filename.concat bindir "ocaml-jupyter-kernel")
+  |> output_string oc ;
+  close_out oc
+
 let read_command cmd =
   let ic = Unix.open_process_in cmd in
   let rec aux acc = match input_line ic with
@@ -41,10 +53,15 @@ let read_command cmd =
   | Unix.WEXITED 0 -> lines
   | _ -> failwith cmd
 
+let read_command_line cmd =
+  match read_command cmd with
+  | [switch] -> switch
+  | msgs -> failwith (sprintf "[%s] %s@." cmd (String.concat "\n" msgs))
+
 let () =
   let home = Sys.getenv "HOME" in
-  let bindir = match read_command "opam config var bin" with
-    | [bindir] -> bindir
-    | msgs -> failwith (sprintf "[opam config var bin] %s@."
-                          (String.concat "\n" msgs)) in
-  main ~output:"kernel.json" ~bindir ~home
+  let switch = read_command_line "opam config var switch" in
+  let bindir = read_command_line "opam config var bin" in
+  let sharedir = read_command_line "opam config var share" in
+  write_kernel_sh ~output:"kernel.sh" ~bindir ~switch ;
+  write_kernelspec_json ~output:"kernel.json" ~bindir ~sharedir ~switch ~home
