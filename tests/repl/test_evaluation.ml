f46091f584242b8256f04aebd631b6c525397b7d
Eugene Lykhovyd <lykhovyd@gmail.com>
2025-12-27 23:54:14 +0000

    clean old ocaml version from tests

--- tests/repl/test_evaluation.ml
+++ tests/repl/test_evaluation.ml
@@ -109,15 +109,11 @@ let test__syntax_error ctxt =
   let status, actual = eval ~count:123 "let let let\nlet" in
   let expected =
     [error ~value:"compile_error"
-       [if Sys.ocaml_version >= "4.08"
-        then "File \"[123]\", line 1, characters 4-7:\
-              \n1 | let let let\
-              \n        ^^^\
-              \nError: Syntax error\n"
-        else "\x1b[32mFile \"[123]\", line 1, characters 4-7:\
-              \n\x1b[31mError: Syntax error\
-              \n\x1b[36m   1: \x1b[30mlet \x1b[4mlet\x1b[0m\x1b[30m let\
-              \n\x1b[36m   2: \x1b[30mlet\x1b[0m\n"]] in
+       ["File \"[123]\", line 1, characters 4-7:\
+         \n1 | let let let\
+         \n        ^^^\
+         \nError: Syntax error\n"
+       ]] in
   assert_equal ~ctxt ~printer:[%show: status] SHELL_ERROR status ;
   assert_equal ~ctxt ~printer:[%show: reply list] (List.map clean_newlines expected) actual
 
@@ -125,14 +121,11 @@ let test__unbound_value ctxt =
   let status, actual = eval ~count:123 "foo 42" in
   let expected =
     [error ~value:"compile_error"
-       [if Sys.ocaml_version >= "4.08"
-        then "File \"[123]\", line 1, characters 0-3:\
-              \n1 | foo 42\
-              \n    ^^^\
-              \nError: Unbound value foo\n"
-        else "\x1b[32mFile \"[123]\", line 1, characters 0-3:\
-              \n\x1b[31mError: Unbound value foo\
-              \n\x1b[36m   1: \x1b[30m\x1b[4mfoo\x1b[0m\x1b[30m 42\x1b[0m\n"]] in
+       [ "File \"[123]\", line 1, characters 0-3:\
+          \n1 | foo 42\
+          \n    ^^^\
+          \nError: Unbound value foo\n"
+       ]] in
   assert_equal ~ctxt ~printer:[%show: status] SHELL_ERROR status ;
   assert_equal ~ctxt ~printer:[%show: reply list] (List.map clean_newlines expected) actual
 
@@ -144,20 +137,11 @@ let test__type_error ctxt =
         then "File \"[123]\", line 1, characters 5-9:\n\
               1 | 42 = true\n         \
               ^^^^\nError: The constructor true has type bool\n       but an expression was expected of type int\n"
-        else if Sys.ocaml_version >= "5.2"
-        then "File \"[123]\", line 1, characters 5-9:\n\
-              1 | 42 = true         \
-              ^^^^Error: This expression has type bool but an expression was expected of type         int"
-        else if Sys.ocaml_version >= "4.08"
-        then "File \"[123]\", line 1, characters 5-9:\
-              \n1 | 42 = true\
-              \n         ^^^^\
-              \nError: This expression has type bool but an expression was expected of type\
-              \n         int\n"
-        else "\x1b[32mFile \"[123]\", line 1, characters 5-9:\
-              \n\x1b[31mError: This expression has type bool but an expression was expected of type\
-              \n         int\
-              \n\x1b[36m   1: \x1b[30m42 = \x1b[4mtrue\x1b[0m\n"]] in
+        else (* assumed >= 5.2 *)
+          "File \"[123]\", line 1, characters 5-9:\n\
+           1 | 42 = true         \
+           ^^^^Error: This expression has type bool but an expression was expected of type         int"
+       ]] in
   assert_equal ~ctxt ~printer:[%show: status] SHELL_ERROR status ;
   assert_equal ~ctxt ~printer:[%show: reply list] (List.map clean_newlines expected) actual
 
@@ -170,92 +154,32 @@ let test__long_error_message ctxt =
        ()" in
   let expected =
     [error ~value:"compile_error"
-       [if Sys.ocaml_version >= "4.08"
-        then "File \"[123]\", line 3, characters 8-11:\
-              \n3 | let c = foo in\
-              \n            ^^^\
-              \nError: Unbound value foo\n"
-        else "\x1b[32mFile \"[123]\", line 3, characters 8-11:\
-              \n\x1b[31mError: Unbound value foo\
-              \n\x1b[36m   2: \x1b[30mlet b = 43 in\
-              \n\x1b[36m   3: \x1b[30mlet c = \x1b[4mfoo\x1b[0m\x1b[30m in\
-              \n\x1b[36m   4: \x1b[30mlet d = 44 in\x1b[0m\n"]] in
+       [ "File \"[123]\", line 3, characters 8-11:\
+          \n3 | let c = foo in\
+          \n            ^^^\
+          \nError: Unbound value foo\n"
+       ]] in
   assert_equal ~ctxt ~printer:[%show: status] SHELL_ERROR status ;
   assert_equal ~ctxt ~printer:[%show: reply list] (List.map clean_newlines expected) actual ;
   let status, actual = eval ~count:123 "List.\n dummy" in
   let expected =
     [error ~value:"compile_error"
-       [if Sys.ocaml_version >= "5.00"
-        then "\nFile \"[123]\", lines 1-2, characters 0-6:\
-              \n1 | List.\
-              \n2 |  dummy\
-              \nError: Unbound value List.dummy\n"
-        else if Sys.ocaml_version >= "4.09"
-        then "File \"[123]\", lines 1-2, characters 0-6:\
-              \n1 | List.\
-              \n2 |  dummy\
-              \nError: Unbound value List.dummy\n"
-        else if Sys.ocaml_version >= "4.08"
-        then "File \"[123]\", line 1, characters 0-12:\
-              \n1 | List.\
-              \n2 |  dummy\
-              \nError: Unbound value List.dummy\n"
-        else "\x1b[32mFile \"[123]\", line 1, characters 0-12:\
-              \n\x1b[31mError: Unbound value List.dummy\
-              \n\x1b[36m   1: \x1b[30m\x1b[4mList.\x1b[0m\
-              \n\x1b[36m   2: \x1b[30m\x1b[4m dummy\x1b[0m\n"]] in
+       ["\nFile \"[123]\", lines 1-2, characters 0-6:\
+         \n1 | List.\
+         \n2 |  dummy\
+         \nError: Unbound value List.dummy\n"
+       ]] in
   assert_equal ~ctxt ~printer:[%show: status] SHELL_ERROR status ;
   assert_equal ~ctxt ~printer:[%show: reply list] (List.map clean_newlines expected) actual
 
 let test__exception ctxt =
   let status, actual = eval "failwith \"FAIL\"" in
   let msg =
-    if Sys.ocaml_version >= "5.2"
-    then"\x1b[31mException: Failure \"FAIL\".\n\
-         Raised at Stdlib.failwith in file \"stdlib.ml\", line 29, characters 17-33\n\
-         Called from <unknown> in file \"[0]\", line 1, characters 0-15\n\
-         Called from Topeval.load_lambda in file \"toplevel/byte/topeval.ml\", line 93, characters 4-14\n\
-         \x1b[0m"
-    else if Sys.ocaml_version >= "5.00"
-    then "\x1b[31mException: Failure \"FAIL\".\n\
-          Raised at Stdlib.failwith in file \"stdlib.ml\", line 29, characters 17-33\n\
-          Called from Topeval.load_lambda in file \"toplevel/byte/topeval.ml\", line 89, characters 4-14\n\
-          \x1b[0m"
-    else if Sys.ocaml_version >= "4.13"
-    then "\x1b[31mException: Failure \"FAIL\".\n\
-          Raised at Stdlib.failwith in file \"stdlib.ml\", line 29, characters 17-33\n\
-          Called from Stdlib__Fun.protect in file \"fun.ml\", line 33, characters 8-15\n\
-          Re-raised at Stdlib__Fun.protect in file \"fun.ml\", line 38, characters 6-52\n\
-          Called from Topeval.load_lambda in file \"toplevel/byte/topeval.ml\", line 89, characters 4-150\n\
-          \x1b[0m"
-    else if Sys.ocaml_version >= "4.12"
-    then "\x1b[31mException: Failure \"FAIL\".\n\
-          Raised at Stdlib.failwith in file \"stdlib.ml\", line 29, characters 17-33\n\
-          Called from Stdlib__fun.protect in file \"fun.ml\", line 33, characters 8-15\n\
-          Re-raised at Stdlib__fun.protect in file \"fun.ml\", line 38, characters 6-52\n\
-          Called from Toploop.load_lambda in file \"toplevel/toploop.ml\", line 212, characters 4-150\n\
-          \x1b[0m"
-    else if Sys.ocaml_version >= "4.11"
-    then "\x1b[31mException: Failure \"FAIL\".\n\
-          Raised at Stdlib.failwith in file \"stdlib.ml\", line 29, characters 17-33\n\
-          Called from Toploop.load_lambda in file \"toplevel/toploop.ml\", line 212, characters 17-27\n\x1b[0m"
-    else if Sys.ocaml_version >= "4.10"
-    then "\x1b[31mException: Failure \"FAIL\".\n\
-          Raised at file \"stdlib.ml\", line 29, characters 22-33\n\
-          Called from file \"toplevel/toploop.ml\", line 212, characters 17-27\n\x1b[0m"
-    else if Sys.ocaml_version >= "4.08"
-    then "\x1b[31mException: Failure \"FAIL\".\n\
-          Raised at file \"stdlib.ml\", line 29, characters 22-33\n\
-          Called from file \"toplevel/toploop.ml\", line 208, characters 17-27\n\x1b[0m"
-    else if Sys.ocaml_version <= "4.02.3"
-    then "\x1b[31mException: Failure \"FAIL\".\n\x1b[0m"
-    else if Sys.ocaml_version <= "4.06.1"
-    then "\x1b[31mException: Failure \"FAIL\".\n\
-          Raised at file \"pervasives.ml\", line 32, characters 22-33\n\
-          Called from file \"toplevel/toploop.ml\", line 180, characters 17-56\n\x1b[0m"
-    else "\x1b[31mException: Failure \"FAIL\".\n\
-          Raised at file \"stdlib.ml\", line 33, characters 22-33\n\
-          Called from file \"toplevel/toploop.ml\", line 180, characters 17-56\n\x1b[0m"
+    "\x1b[31mException: Failure \"FAIL\".\n\
+     Raised at Stdlib.failwith in file \"stdlib.ml\", line 29, characters 17-33\n\
+     Called from <unknown> in file \"[0]\", line 1, characters 0-15\n\
+     Called from Topeval.load_lambda in file \"toplevel/byte/topeval.ml\", line 93, characters 4-14\n\
+     \x1b[0m"
   in
   let expected = [error ~value:"runtime_error" [msg]] in
   assert_equal ~ctxt ~printer:[%show: status] SHELL_ERROR status ;
@@ -263,10 +187,7 @@ let test__exception ctxt =
 
 let test__unknown_directive ctxt =
   let status, actual = eval "#foo" in
-  let msg =
-    if Sys.ocaml_version >= "5.2"
-    then "\x1b[31mUnknown directive foo.\x1b[0m"
-    else "\x1b[31mUnknown directive `foo'.\x1b[0m" in
+  let msg = "\x1b[31mUnknown directive foo.\x1b[0m" in
   let expected = [error ~value:"runtime_error"
                     [msg]] in
   assert_equal ~ctxt ~printer:[%show: status] SHELL_ERROR status ;
